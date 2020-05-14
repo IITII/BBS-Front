133cb5be96747924f477f749f3509f4a79afbc98
IITII <ccmejx@gmail.com>
2020-05-14 22:01:46 +0800

    1. Add page 2. use lodash as dev dependencies

---
+++ src/components/success.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-alert v-if="success" variant="success">{{ info }}</b-alert>
+</template>
+
+<script>
+  // 自定义组件，使用 prop 进行传参
+  export default {
+    name: "success",
+    // ES6写法，即 key 名称与传入参数名称重复即可省略
+    // error: error 等效于 'error'
+    props: [
+      'success',
+      'info'
+    ]
+  }
+</script>
+
+<style scoped>
+  .alert {
+    margin-top: 3%;
+    margin-bottom: 3%;
+  }
+</style>
